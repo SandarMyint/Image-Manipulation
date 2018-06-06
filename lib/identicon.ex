baf7325cdde54f85315a0cf09528c5e182b34807
Asus <sandarmyint5897@gmail.com>
2018-06-06 16:08:54 +0630

    Grid Structure

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -7,6 +7,7 @@ defmodule Identicon do
     input 
     |> hash_input
     |> pick_color
+    |> built_grid
   end
 
   @doc """
@@ -30,5 +31,20 @@ defmodule Identicon do
   def pick_color(image) do
     %Identicon.Image{hex: [r,g,b|_tail]} = image
     %Identicon.Image{image|color: [r,g,b]}
+  end
+
+  def built_grid(%Identicon.Image{hex: hex} = image) do
+    grid = 
+      hex
+      |> Enum.chunk(3)
+      |> Enum.map(&mirror_row/1)
+      |> List.flatten
+      |> Enum.with_index
+    %Identicon.Image{image | grid: grid}
+  end
+
+  def mirror_row(row) do
+    [first, second | _tail] = row
+    row ++ [second, first]
   end
 end
